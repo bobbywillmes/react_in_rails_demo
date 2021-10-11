60e6b42ab39733f02f70f35f7a6ac898361650a6
Bobby Willmes <bobbywillmes@gmail.com>
2021-10-11 15:09:05 -0700

    react router

--- app/javascript/src/demo.jsx
+++ app/javascript/src/demo.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Layout from './layout';
+
+const SubRoute = ({match}) => <p className="p-2 mt-2 border border-light">{match.url}</p>
 const Demo = () => (
-  <Layout>
-    <h1>Demo page</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo unde modi accusantium aut iure nesciunt consectetur sit ex iste perspiciatis animi esse ipsum dolorum, iusto, quibusdam molestiae, suscipit laborum odio!</p>
-  </Layout>
+  <Router>
+    <Layout>
+      <h1>Demo page</h1>
+      <ul className="nav">
+        <li className="nav-item">
+          <Link className="nav-link" to="/demo/a">Link A</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/demo/b">Link B</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/demo/c">Link C</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/demo/a" exact component={SubRoute} />
+        <Route path="/demo/b" exact component={SubRoute} />
+        <Route path="/demo/c" exact component={SubRoute} />
+      </Switch>
+    </Layout>
+  </Router>
 )
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
